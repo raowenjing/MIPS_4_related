881d8db32f4c39adc447685e1722b50ba0fb16e4
raowenjing <41601363+raowenjing@users.noreply.github.com>
2019-09-16 17:23:36 -0500

    loop + check 1
    tested, working.

--- loop.asm
+++ loop.asm
@@ -4,13 +4,31 @@
 # $9 for n
 # $10 for addr
 
+# $23 as temp
+
 addi $8, $0, 100
 addi $9, $0, 1
 ori $10, $0, 0x2000
 
 loop_100:
 
+# check 1: n is 4x
+# idea: n AND 3, check result (0: 4x; 1: otherwise)
+andi $23, $9, 3
+beq $23, $0, related_4
+
+
+
+# UNrelated to 4: does this
 sw $9, 0($10)
+j out
+
+# related to 4: does the following
+related_4:
+addi $23, $0, -1
+sw $23, 0($10)
+
+out:
 
 addi $9, $9, 1
 addi $10, $10, 4
